74228cbb0c12e145bc18d0fa5516a03de102e067
Gianmarco Garrisi <gianmarco.garrisi@studenti.polito.it>
2019-05-24 18:22:02 +0200

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+/*
+ *  Copyright © 2019 Gianmarco Garrisi
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use std::io::{Result, Write};
+use std::fmt::Display;
+
+/// This trait lets you write an iterator into a writer, with each item on a line.
+pub trait LinesWriter {
+    fn write_lines<I>(&mut self, iterator: I) -> Result<usize>
+    where I: IntoIterator,
+    I::Item: Display;
+}
+
+impl<T> LinesWriter for T
+where T: Write {
+    fn write_lines<I>(&mut self, iterator: I) -> Result<usize> 
+    where I: IntoIterator,
+    I::Item: Display {
+        let mut res = 0;
+        for el in iterator {
+            writeln!(self, "{}", el)?;
+            res+=1;
+        }
+        Ok(res)
+    }
+}
